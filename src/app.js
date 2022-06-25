ff0b363bbbfe4ac19a7e80c4bc3153cbcf8b7fbe
Snizhanna-btv <ravenblack1825@gmail.com>
2022-06-26 02:13:15 +0300

    Added final features

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// convert celsius to fahrenheit and vice versa
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   celsiusLink.classList.remove("active");
@@ -23,6 +24,8 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
+// display last updated info
+
 function lastUpdated(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -47,6 +50,61 @@ function lastUpdated(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+// display forecast for the next days
+
+function editFormOfDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-day">${editFormOfDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "c66d23310349377a45e61445f0813304";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+// display details about weather
+
 function displayWeather(response) {
   let temperature = document.querySelector("#temperature");
   let city = document.querySelector("#city");
@@ -69,7 +127,10 @@ function displayWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   icon.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
+
+// search cite and handle user input
 
 function search(city) {
   let apiKey = "c66d23310349377a45e61445f0813304";
